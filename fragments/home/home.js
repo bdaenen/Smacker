2f8c6cb6ef21633ba3851744b2d67f8fd5585e0e
benno.daenen <benno.daenen@tobania.be>
2018-07-19 00:26:18 +0200

    Improved form styling and fixed bug with session variables expiring but server-side session still running.

--- fragments/home/home.js
+++ fragments/home/home.js
@@ -1,4 +1,8 @@
 (function() {
     'use strict';
-
+    var app = window.app;
+    var user = app.getUser();
+    if (user) {
+        app.$mainContent.find('#user-tag').text(', ' + user.tag);
+    }
 }());
